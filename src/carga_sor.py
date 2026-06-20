e8fcee85683a95b7f19b1c2e21fe293099ec6add
Leandro <leandroscj2012@gmail.com>
2026-06-20 01:31:27 -0300

    refatora função de carga para aplicar metadados e corrige nome de bucket

--- src/carga_sor.py
+++ src/carga_sor.py
@@ -1,23 +1,23 @@
 from pyspark.sql.functions import lit, col
-from utils.functions import get_metadata_table, write_bucket
+from utils.functions import apply_metadata_table, write_bucket
 
-def exec_carga_sor(competencia, path_entrada, table):
-
+def exec_carga_sor(competencia, path_entrada, table, schema_destino):
     df = spark.read.parquet(path_entrada)
     df = df.withColumn("competencia", lit(competencia))
-    get_metadata_table(table, df)
+    df = apply_metadata_table(schema_destino, df)
 
     write_bucket(df, table)
 
 if __name__ == "__main__":
-    BUCKET = "datalake-ifood-case"
+    BUCKET = "ifood-case-406207085720"
     VEHICLE_COMPETENCIAS = {
         "yellow": ["202301", "202302","202303", "202304", "202305"],
         "green": ["202301", "202302","202303", "202304", "202305"]
     }
-
+    
     for vehicle, competencias in VEHICLE_COMPETENCIAS.items():
+        table = f"sor.{vehicle}_taxi"
+        schema_destino = spark.table(table).limit(0).schema
         for competencia in competencias:
             path = f"s3a://{BUCKET}/landing/competencia={competencia}/vehicle={vehicle}"
-            table = f"sor.{vehicle}_taxi"
-            exec_carga_sor(competencia, path, table)
+            exec_carga_sor(competencia, path, table, schema_destino)
--- src/utils/functions.py
+++ src/utils/functions.py
@@ -1,8 +1,6 @@
 from pyspark.sql.functions import lit, col
 
-def get_metadata_table(target_table, df):
-    schema_destino = spark.table(target_table).limit(0).schema
-
+def apply_metadata_table(schema_destino, df):
     colunas_formatadas = []
     for campo in schema_destino:
         nome_coluna = campo.name
@@ -11,6 +9,9 @@ def get_metadata_table(target_table, df):
             colunas_formatadas.append(col(nome_coluna).cast(tipo_coluna))  
         else:
             colunas_formatadas.append(lit(None).cast(tipo_coluna).alias(nome_coluna))
+    
+    df = df.select(*colunas_formatadas)
+    return df
 
 def write_bucket(df, target_table):
     df.write \
