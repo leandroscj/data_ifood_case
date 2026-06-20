3a0ada57e1ea531c849e08930410ce4b309b51c8
Leandro <leandroscj2012@gmail.com>
2026-06-20 02:58:20 -0300

    refatora função exec_carga_sot para aceitar parâmetros de tabelas e ajusta chamada; adiciona nova função exec_carga_spec para carga de dados na tabela de especificações

--- src/carga_sot.py
+++ src/carga_sot.py
@@ -1,12 +1,11 @@
-from pyspark.sql import SparkSession
 from pyspark.sql.functions import lit, col
 from utils.functions import apply_metadata_table, write_bucket
 
-def exec_carga_sot(sot_table):
+def exec_carga_sot(sot_table, table_yellow, table_green):
     schema_destino = spark.table(sot_table).limit(0).schema
 
-    df_yellow = spark.table("sor.yellow_taxi").filter(col("competencia").isin(VEHICLE_COMPETENCIAS["yellow"]))
-    df_green = spark.table("sor.green_taxi").filter(col("competencia").isin(VEHICLE_COMPETENCIAS["green"]))
+    df_yellow = spark.table(table_yellow).filter(col("competencia").isin(VEHICLE_COMPETENCIAS["yellow"]))
+    df_green = spark.table(table_green).filter(col("competencia").isin(VEHICLE_COMPETENCIAS["green"]))
     
     df_green = df_green.withColumnRenamed("lpep_pickup_datetime", "tpep_pickup_datetime") \
         .withColumnRenamed("lpep_dropoff_datetime", "tpep_dropoff_datetime")
@@ -24,6 +23,8 @@ if __name__ == "__main__":
         "yellow": ["202301", "202302","202303", "202304", "202305"],
         "green": ["202301", "202302","202303", "202304", "202305"]
     }
-    TABLE = "sot.green_taxi"
+    TABLE = "sot.ny_taxi"
+    TABLE_YELLOW = "sot.yellow_taxi"
+    TABLE_GREEN = "sot.green_taxi"
 
-    exec_carga_sot(TABLE)
+    exec_carga_sot(TABLE, TABLE_YELLOW, TABLE_GREEN)
---
+++ src/carga_spec.py
@@ -0,0 +1,35 @@
+from pyspark.sql.functions import lit, col, sum as spark_sum
+from utils.functions import apply_metadata_table, write_bucket
+
+def exec_carga_spec(spec_table, table_sot):
+    schema_destino = spark.table(spec_table).limit(0).schema
+
+    filtro = (
+        (col("vehicle") == "yellow") & col("competencia").isin(VEHICLE_COMPETENCIAS["yellow"]) 
+        | (col("vehicle") == "green") & col("competencia").isin(VEHICLE_COMPETENCIAS["green"]) 
+    )
+
+    df_sot = spark.table(table_sot).filter(filtro)
+    df_sot = df_sot.groupBy(
+            "VendorID",
+            "passenger_count",
+            "tpep_pickup_datetime",
+            "tpep_dropoff_datetime",
+            "vehicle",
+            "competencia"
+        ).agg(spark_sum("total_amount").alias("total_amount"))
+
+
+    union_df = apply_metadata_table(schema_destino, df_sot)
+
+    write_bucket(union_df, spec_table)
+
+if __name__ == "__main__":
+    VEHICLE_COMPETENCIAS = {
+        "yellow": ["202301", "202302","202303", "202304", "202305"],
+        "green": ["202301", "202302","202303", "202304", "202305"]
+    }
+    TABLE = "spec.ny_taxi_consumers"
+    TABLE_SOT = "sot.ny_taxi"
+
+    exec_carga_spec(TABLE, TABLE_SOT)
