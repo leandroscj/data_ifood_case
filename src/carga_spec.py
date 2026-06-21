287085f2221ace355fd96a0967e8296a916686f2
Leandro <leandroscj2012@gmail.com>
2026-06-20 21:46:09 -0300

    fix: path do arquivo de parametros

--- src/carga_spec.py
+++ src/carga_spec.py
@@ -1,4 +1,3 @@
-import os
 import json
 from pyspark.sql.functions import lit, col, sum as spark_sum
 from utils.functions import apply_metadata_table, write_bucket
@@ -27,7 +26,7 @@ def exec_carga_spec(spec_table, table_sot):
     write_bucket(union_df, spec_table)
 
 if __name__ == "__main__":
-    path_param = os.path.join(os.path.dirname(os.path.abspath(__file__)), "param.json")
+    path_param = "param.json"
     with open(path_param, "r") as f:
         params = json.load(f)
 
